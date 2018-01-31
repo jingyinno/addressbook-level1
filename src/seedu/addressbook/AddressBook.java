72347bba6b8e3f6daece58f1429a522bd93ac736
Jing <ojyy73@gmail.com>
2018-01-31 10:21:50 +0800

    Decompose Conditional Extract inputLine checker

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -602,13 +602,22 @@ public class AddressBook {
         System.out.print(LINE_PREFIX + "Enter command: ");
         String inputLine = SCANNER.nextLine();
         // silently consume all blank and comment lines
-        while (inputLine.trim().isEmpty() || inputLine.trim().charAt(0) == INPUT_COMMENT_MARKER) {
+        while (isValidInput(inputLine)) {
             inputLine = SCANNER.nextLine();
         }
         return inputLine;
     }
 
-   /*
+    /**
+     * Returns true if inputLine is valid
+     * @param inputLine
+     * @return
+     */
+    private static boolean isValidInput(String inputLine) {
+        return inputLine.trim().isEmpty() || inputLine.trim().charAt(0) == INPUT_COMMENT_MARKER;
+    }
+
+    /*
     * NOTE : =============================================================
     * Note how the method below uses Java 'Varargs' feature so that the
     * method can accept a varying number of message parameters.
